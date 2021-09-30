57241863469b74b8e261981aca925e774c7b78d7
Tanguay <e1974422@cmaisonneuve.qc.ca>
2021-09-30 11:32:04 -0400

    Modifications au footer

--- captchaTheme/footer.php
+++ captchaTheme/footer.php
@@ -11,24 +11,40 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'under' ) ); ?>">
-				<?php
+<footer id="colophon" class="site-footer">
+
+	<div class="site-info">
+		<div class="pied">
+		<div class="reseaux">
+<a href="https://fr-ca.facebook.com/maisonneuvetim/" title="Facebook (TIM)"><i class="fab fa-facebook-f"></i></a>
+<a href="https://www.instagram.com/maisonneuvetim/?hl=fr" title="Instagram (TIM)"><i class="fab fa-instagram"></i></a>
+<a href="https://twitter.com/cdemaisonneuve?lang=fr" title="Twitter (Collège)"><i class="fab fa-twitter"></i></a>
+<a href="https://www.youtube.com/channel/UCEP_4G7I40t6if8BAu0TNMA" title="YouTube (TIM)"><i class="fab fa-youtube"></i></a>
+</div>
+		<div class="addresse">
+			<p>3800, rue sherbrooke est</p>
+			<p>Montréal, Qc</p>
+			<p>H1X 2A2</p>
+		</div>
+
+</div>
+		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'under' ) ); ?>">
+			<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'under' ), 'WordPress' );
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
+		</a>
+		<span class="sep"> | </span>
+		<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'under' ), 'under', '<a href="http://underscores.me/">Abinassh</a>' );
 				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
