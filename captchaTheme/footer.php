4fba0ab7f1c706e7e5595c65196e3cd8a5583adb
Arydup <71138810+Arydup@users.noreply.github.com>
2021-10-24 20:38:16 -0400

    mode sombre début

--- captchaTheme/footer.php
+++ captchaTheme/footer.php
@@ -15,19 +15,17 @@
 
 	<div class="site-info">
 		<div class="pied">
-		<div class="reseaux">
-<a href="https://fr-ca.facebook.com/maisonneuvetim/" title="Facebook (TIM)"><i class="fab fa-facebook-f"></i></a>
-<a href="https://www.instagram.com/maisonneuvetim/?hl=fr" title="Instagram (TIM)"><i class="fab fa-instagram"></i></a>
-<a href="https://twitter.com/cdemaisonneuve?lang=fr" title="Twitter (Collège)"><i class="fab fa-twitter"></i></a>
-<a href="https://www.youtube.com/channel/UCEP_4G7I40t6if8BAu0TNMA" title="YouTube (TIM)"><i class="fab fa-youtube"></i></a>
-</div>
-		<div class="addresse">
-			<p>3800, rue sherbrooke est</p>
-			<p>Montréal, Qc</p>
-			<p>H1X 2A2</p>
+			<div class="reseaux">
+				<a href="https://fr-ca.facebook.com/maisonneuvetim/" title="Facebook (TIM)"><i class="fab fa-facebook-f"></i></a>
+				<a href="https://www.instagram.com/maisonneuvetim/?hl=fr" title="Instagram (TIM)"><i class="fab fa-instagram"></i></a>
+				<a href="https://twitter.com/cdemaisonneuve?lang=fr" title="Twitter (Collège)"><i class="fab fa-twitter"></i></a>
+				<a href="https://www.youtube.com/channel/UCEP_4G7I40t6if8BAu0TNMA" title="YouTube (TIM)"><i class="fab fa-youtube"></i></a>
+			</div>
+			<div class="adresse">
+				<p>3800, rue sherbrooke est,<br>Montréal,Qc,<br>H1X 2A2</p>
+			</div>
+			<a class="piedPageLogo" href="https://www.cmaisonneuve.qc.ca/">...</a>
 		</div>
-
-</div>
 		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'under' ) ); ?>">
 			<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
@@ -39,7 +37,8 @@
 				/* translators: 1: Theme name, 2: Theme author. */
 				//printf( esc_html__( 'Theme: %1$s by %2$s.', 'under' ), 'under', '<a href="http://underscores.me/">Abinassh</a>' );
 				?>
-		<p>Un thème de : <span class="nomAuteurs">Aryane Duperron, Stéphanie Plante, Abinassh Kanesh et Cyrile Tanguay</span></p>
+		<p class="nomAuteurs">Conception et développement web par Aryane Duperron, Stéphanie Plante, Abinassh Kanesh & Cyrile Tanguay</p>
+		<p>Tous droits réservés ©2021 - Techniques d'intégration multimédia - Collège de Maisonneuve</p>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
