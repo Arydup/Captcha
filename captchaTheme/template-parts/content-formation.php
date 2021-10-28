0d5197ebfd51c8b6902e8cc16e684fc85df26129
Kanesh <e1949554@cmaisonneuve.qc.ca>
2021-10-28 12:56:02 -0400

    début grille de cours

---
+++ captchaTheme/template-parts/content-formation.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscore
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<p> </p>' );
+		$chaine_titre.= get_the_title(); 
+		//echo $chaine_titre;
+		?>
+	</header><!-- .entry-header -->
+
+	<?php //under_post_thumbnail(); ?>
+
+	<div class="entry-content">
+
+	</div><!-- .entry-content -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
