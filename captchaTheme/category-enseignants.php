b5c317c3eb537dc5669d952420e43fd476eed495
Steph017 <e1821985@cmaisonneuve.qc.ca>
2021-11-18 12:07:12 -0500

    continuation de la page enseignant.

--- captchaTheme/category-enseignants.php
+++ captchaTheme/category-enseignants.php
@@ -15,11 +15,14 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
 			
-		
 			</header><!-- .page-header -->
 
-			
+			<section class='PageEnseignants'>
 			<?php
 
 			/* Start the Loop */
@@ -36,7 +39,8 @@ get_header();
 			get_template_part( 'template-parts/content', 'enseignants' );
 
 			endwhile;
-
+			
+			?></section><?php
 		endif;
 		?>
 
