f0a41eb5df4d49181531ebd81a2824e9c0aece60
CyrilleTanguay <e1974422@cmaisonneuve.qc.ca>
2021-12-10 17:11:36 -0500

    Autres corrections

--- captchaTheme/template-parts/content-avenir.php
+++ captchaTheme/template-parts/content-avenir.php
@@ -1,26 +1,28 @@
 <!-- Article (partie Perspectives d'emploi) -->
-<article class="avenirPartie" >
+<article class="avenirPartie">
     <!-- Image -->
     <section class="avenirPhoto">
-<img src="https://www.aappq.qc.ca/content/cache/0x0/10_maisondesetudiantsets_menkesshoonerdagenaisletourneuxarchitectes_creditstephanebrugger.jpg" alt="">
+        <img src="https://www.aappq.qc.ca/content/cache/0x0/10_maisondesetudiantsets_menkesshoonerdagenaisletourneuxarchitectes_creditstephanebrugger.jpg"
+            alt="">
     </section>
     <!-- Texte -->
-<section class="avenirTexte">
-<h3>Perspectives d'emploi</h3>
-<p>Nos étudiants peuvent devenir:</p>
-<ul>
-    <li>Spécialiste en expérience utilisateur (UX)</li>
-    <li>Intégrateur multimédia</li>
-    <li>Concepteur de niveau de jeux vidéos</li>
-    <li>Développeur d’applications mobiles</li>
-    <li>Animateur 2D/3D</li>
-    <li>Chargé de projets numériques</li>
-    <li>Designer graphique et infographiste</li>
-    <li>Monteur vidéo</li><li>Développeur Front-end</li>
-    <li>Programmeur de jeux vidéos</li>
-    <li>Artiste 3D</li>
-</ul>
-</section>
+    <section class="avenirTexte">
+        <h3>Perspectives d'emploi</h3>
+        <p>Nos étudiants peuvent devenir:</p>
+        <ul>
+            <li>Spécialiste en expérience utilisateur (UX)</li>
+            <li>Intégrateur multimédia</li>
+            <li>Concepteur de niveau de jeux vidéos</li>
+            <li>Développeur d’applications mobiles</li>
+            <li>Animateur 2D/3D</li>
+            <li>Chargé de projets numériques</li>
+            <li>Designer graphique et infographiste</li>
+            <li>Monteur vidéo</li>
+            <li>Développeur Front-end</li>
+            <li>Programmeur de jeux vidéos</li>
+            <li>Artiste 3D</li>
+        </ul>
+    </section>
 </article>
 <!-- Ligne -->
 <br>
@@ -28,78 +30,96 @@
 <br>
 <!-- Article (partie Perspectives universitaires) -->
 
-<article class="avenirPartie" >
+<article class="avenirPartie">
     <!-- Image -->
     <section class="avenirPhoto">
-<img src="https://upload.wikimedia.org/wikipedia/commons/a/af/UQAT_campusRouyn-Noranada.jpg" alt="">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/af/UQAT_campusRouyn-Noranada.jpg" alt="">
     </section>
     <!-- Texte -->
-<section class="avenirTexte">
-<h3>Perspectives universitaires</h3>
-<br>
-<a href="https://www.uqat.ca/etudes/creation-et-nouveaux-medias/baccalaureat-en-creation-numerique-profils-informatique-cinema-creation-3d-technologie-web/">DEC-BAC en création numérique – concentration Cinéma, Création 3D ou Technologie Web à l’UQAT (Rouyn-Noranda)</a>
-<br>
-<p>Le baccalauréat en création numérique vise à développer chez l’étudiant des
-habiletés créatives et conceptuelles dans plusieurs disciplines convergentes dont la
-création cinématographique et vidéographique, le design d’images de synthèse et
-l’intégration Web, ainsi que la communication graphique.</p>
-</section>
+    <section class="avenirTexte">
+        <h3>Perspectives universitaires</h3>
+        <br>
+        <a
+            href="https://www.uqat.ca/etudes/creation-et-nouveaux-medias/baccalaureat-en-creation-numerique-profils-informatique-cinema-creation-3d-technologie-web/">DEC-BAC
+            en création numérique – concentration Cinéma, Création 3D ou Technologie Web à l’UQAT (Rouyn-Noranda)</a>
+        <br>
+        <p>Le baccalauréat en création numérique vise à développer chez l’étudiant des
+            habiletés créatives et conceptuelles dans plusieurs disciplines convergentes dont la
+            création cinématographique et vidéographique, le design d’images de synthèse et
+            l’intégration Web, ainsi que la communication graphique.</p>
+    </section>
 </article>
 <br>
 <hr class="lignePage">
 <br>
-<article class="avenirPartie" >
-<!-- Image -->
+<article class="avenirPartie">
+    <!-- Image -->
     <section class="avenirPhoto">
-<img src="https://www.iheartradio.ca/image/policy:1.3204075:1504034114/UQAT-en-ete.jpg?f=default&$p$f=f6894cb" alt="">
+        <img src="https://www.iheartradio.ca/image/policy:1.3204075:1504034114/UQAT-en-ete.jpg?f=default&$p$f=f6894cb"
+            alt="">
     </section>
     <!-- Texte -->
-<section class="avenirTexte">
-<a rel="noreferrer noopener" href="https://www.uqat.ca/etudes/creation-et-nouveaux-medias/baccalaureat-en-creation-de-jeux-video-profils-art-et-design/" target="_blank">DEC-BAC en création de jeux vidéo – Intégration logicielle à l’UQAT (Montréal)</a>
-<br>
-<br>
-<p>Le baccalauréat en création de jeux vidéo est une formation sur mesure pour la
-création et le développement de jeux vidéo. Il vise le développement de la
-créativité, de l’autonomie et du travail d’équipe par la création et l’étude d’œuvres
-vidéoludiques.</p>
-</section>
+    <section class="avenirTexte">
+        <a rel="noreferrer noopener"
+            href="https://www.uqat.ca/etudes/creation-et-nouveaux-medias/baccalaureat-en-creation-de-jeux-video-profils-art-et-design/"
+            target="_blank">DEC-BAC en création de jeux vidéo – Intégration logicielle à l’UQAT (Montréal)</a>
+        <br>
+        <br>
+        <p>Le baccalauréat en création de jeux vidéo est une formation sur mesure pour la
+            création et le développement de jeux vidéo. Il vise le développement de la
+            créativité, de l’autonomie et du travail d’équipe par la création et l’étude d’œuvres
+            vidéoludiques.</p>
+    </section>
 </article>
 <br>
 <hr class="lignePage">
 <br>
-<article class="avenirPartie" >
-<!-- Image -->
+<article class="avenirPartie">
+    <!-- Image -->
     <section class="avenirPhoto">
-<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Pavillon_Judith-Jasmin_UQAM_2.jpg/800px-Pavillon_Judith-Jasmin_UQAM_2.jpg" alt="">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Pavillon_Judith-Jasmin_UQAM_2.jpg/800px-Pavillon_Judith-Jasmin_UQAM_2.jpg"
+            alt="">
     </section>
     <!-- Texte -->
-<section class="avenirTexte">
-<a href="https://etudier.uqam.ca/programme/baccalaureat-communication-medias-interactifs">B. en communication (création médias – médias interactifs) à l’UQAM</a>
-<br>
-<br>
-<p>Ce programme a pour objectif de former des intervenants compétents dans le domaine des communications médiatiques. Plus spécifiquement : former des spécialistes en interactivité et des concepteurs en nouveaux médias. On recherche des individus créatifs, débrouillards, et articulés, prêts à s'inscrire dans les exigences d'innovation continuelle du média et à participer à son évolution.</p>
-</section>
+    <section class="avenirTexte">
+        <a href="https://etudier.uqam.ca/programme/baccalaureat-communication-medias-interactifs">B. en communication
+            (création médias – médias interactifs) à l’UQAM</a>
+        <br>
+        <br>
+        <p>Ce programme a pour objectif de former des intervenants compétents dans le domaine des communications
+            médiatiques. Plus spécifiquement : former des spécialistes en interactivité et des concepteurs en nouveaux
+            médias. On recherche des individus créatifs, débrouillards, et articulés, prêts à s'inscrire dans les
+            exigences d'innovation continuelle du média et à participer à son évolution.</p>
+    </section>
 </article>
 <br>
 <!-- Ligne -->
 <hr class="lignePage">
 <br>
-<article class="avenirPartie" >
-<!-- Image -->
+<article class="avenirPartie">
+    <!-- Image -->
     <section class="avenirPhoto">
-<img src="https://www.aappq.qc.ca/content/cache/0x0/10_maisondesetudiantsets_menkesshoonerdagenaisletourneuxarchitectes_creditstephanebrugger.jpg" alt="">
+        <img src="https://www.aappq.qc.ca/content/cache/0x0/10_maisondesetudiantsets_menkesshoonerdagenaisletourneuxarchitectes_creditstephanebrugger.jpg"
+            alt="">
     </section>
     <!-- Texte -->
-<section class="avenirTexte">
-<a href="https://www.etsmtl.ca/etudes/premier-cycle/Baccalaureat-genie-des-TI">Bac en génie des technologies de l’information</a> ou en <a href="https://www.etsmtl.ca/etudes/premier-cycle/Baccalaureat-genie-logiciel">génie logiciel à l’ÉTS</a>
-<br>
-<br>
-<h4>Génie en techniques de l'information</h4>
-<p>Ce baccalauréat vous spécialisera en conception de systèmes d’information pour l'entreprise. Vous travaillerez à la conception, à l'adaptation et à la mise en place d’infrastructures matérielles et logicielles dans le but de répondre aux besoins des entreprises et d'améliorer leur rendement.</p>
-<br>
-<h4>Génie logiciel</h4>
-<p>Le programme de baccalauréat en génie logiciel vous apprendra entre autres à concevoir et à développer des applications commerciales, des systèmes de contrôle de réseau et des jeux vidéos. Il vous préparera à occuper des postes notamment d’ingénieur logiciel, de responsable du développement, d'analyste, de consultant ou de concepteur de logiciel.</p>
-</section>
+    <section class="avenirTexte">
+        <a href="https://www.etsmtl.ca/etudes/premier-cycle/Baccalaureat-genie-des-TI">Bac en génie des technologies de
+            l’information</a> ou en <a
+            href="https://www.etsmtl.ca/etudes/premier-cycle/Baccalaureat-genie-logiciel">génie logiciel à l’ÉTS</a>
+        <br>
+        <br>
+        <h4>Génie en techniques de l'information</h4>
+        <p>Ce baccalauréat vous spécialisera en conception de systèmes d’information pour l'entreprise. Vous
+            travaillerez à la conception, à l'adaptation et à la mise en place d’infrastructures matérielles et
+            logicielles dans le but de répondre aux besoins des entreprises et d'améliorer leur rendement.</p>
+        <br>
+        <h4>Génie logiciel</h4>
+        <p>Le programme de baccalauréat en génie logiciel vous apprendra entre autres à concevoir et à développer des
+            applications commerciales, des systèmes de contrôle de réseau et des jeux vidéos. Il vous préparera à
+            occuper des postes notamment d’ingénieur logiciel, de responsable du développement, d'analyste, de
+            consultant ou de concepteur de logiciel.</p>
+    </section>
 </article>
 <!-- Marge pour que le footer ne masque pas le texte -->
 <div style="margin-bottom: 50px;"></div>
